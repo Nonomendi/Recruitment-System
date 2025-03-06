6a89ca1ed2a23bdc1cc2ec5cf5198fb199cba206
Nolundi Mendi <nolundimendi4@gmail.com>
2025-03-06 15:04:36 -0800

    built the apis ,main class and the serve , i need to connect java with mysql in order to ru the project

---
+++ RecruitmentSystem/src/main/java/com/example/backend/cotroller/BCryptPasswordEncoder.java
@@ -0,0 +1,15 @@
+//package com.example.backend.cotroller;
+//
+//public class BCryptPasswordEncoder {
+//
+//    public boolean matches(String password, String password2) {
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'matches'");
+//    }
+//
+//    public String encode(String password) {
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'encode'");
+//    }
+//
+//}
